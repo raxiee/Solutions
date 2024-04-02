8fe2175f80881e152843477c83edf9b2c041c0e3
raxiee <pellehartig@hotmail.com>
2024-04-02 14:56:00 +0200

    Mapper

--- 01_basics/S0180_morris_exercise.py
+++ 01_basics/S0180_morris_exercise.py
@@ -31,28 +31,17 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil.
 """
-def morris(turns, sleepiness, thirst, hunger, whiskey, gold):
-    sleepiness = 0
-    thirst = 0
-    hunger = 0
-    whiskey = 0
-    gold = 0
-    sleep(0, 0, 0, 0, 0,)
-    print('Morris ended the game with', gold, 'gold after', turns, 'turns.\n\n' 'His attributes were at the following levels:')
-    print('sleepiness =', sleepiness)
-    print('thirst =',thirst)
-    print('hunger =',hunger)
-    print('whiskey =',whiskey)
 
-def sleep(sleepiness, thirst, hunger, whiskey, gold):
-    sleepiness -= 10
+def sleep(sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
+    sleepiness -= 10 if sleepiness >= 10 else sleepiness
     thirst += 1
     hunger += 1
     whiskey += 0
     gold += 0
     return sleepiness, thirst, hunger, whiskey, gold
 
-def mine(sleepiness, thirst, hunger, whiskey, gold):
+
+def mine(sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
     sleepiness += 5
     thirst += 5
     hunger += 5
@@ -60,15 +49,17 @@ def mine(sleepiness, thirst, hunger, whiskey, gold):
     gold += 5
     return sleepiness, thirst, hunger, whiskey, gold
 
-def eat(sleepiness, thirst, hunger, whiskey, gold):
-    sleepiness += 5
-    thirst -= 5
-    hunger -= 20
-    whiskey += 0
+
+def eat(sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
+    sleepiness += 5 if sleepiness >= 5 else sleepiness
+    thirst -= 5 if thirst >= 5 else thirst
+    hunger -= 20 if hunger >= 20 else hunger
+    whiskey += 0 if whiskey >= 0 else whiskey
     gold -= 2
     return sleepiness, thirst, hunger, whiskey, gold
 
-def buy_whiskey(sleepiness, thirst, hunger, whiskey, gold):
+
+def buy_whiskey(sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
     sleepiness += 5
     thirst += 1
     hunger += 1
@@ -76,7 +67,8 @@ def buy_whiskey(sleepiness, thirst, hunger, whiskey, gold):
     gold -= 1
     return sleepiness, thirst, hunger, whiskey, gold
 
-def drink(sleepiness, thirst, hunger, whiskey, gold):
+
+def drink(sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
     sleepiness += 5
     thirst -= 15
     hunger -= 1
@@ -84,15 +76,71 @@ def drink(sleepiness, thirst, hunger, whiskey, gold):
     gold += 0
     return sleepiness, thirst, hunger, whiskey, gold
 
+def print_attributes(sleepiness, thirst, hunger, whiskey, gold):
+    print('sleepiness =', sleepiness)
+    print('thirst =', thirst)
+    print('hunger =', hunger)
+    print('whiskey =', whiskey)
+
+def morris_in_recovery(turns, sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
+    turns_done = 0
+    for i in range(turns):
+        turns_done = i
+        if sleepiness > 100:
+            print('Oh no, you killed poor Morris! When he passed away from sleep deprivation after', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif thirst > 100:
+            print('Oh no, you killed poor Morris! When he died of thirst after', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif hunger > 100:
+            print('Oh no, you killed poor Morris! When he died of starvation after', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif gold < 0:
+            print('Oh no, you bankrupted poor Morris! When you caused him to go broke after', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif sleepiness >= 95:
+            sleepiness, thirst, hunger, whiskey, gold = sleep(sleepiness, thirst, hunger, whiskey, gold)
+        elif thirst >= 95:
+            sleepiness, thirst, hunger, whiskey, gold = eat(sleepiness, thirst, hunger, whiskey, gold)
+        else:
+            sleepiness, thirst, hunger, whiskey, gold = mine(sleepiness, thirst, hunger, whiskey, gold)
+
+    return sleepiness, thirst, hunger, whiskey, gold, turns, turns_done
+
+
+def morris_functional_alcoholic(turns, sleepiness=0, thirst=0, hunger=0, whiskey=0, gold=0):
+    turns_done = 0
+    for i in range(turns):
+        turns_done = i
+        if sleepiness > 100:
+            print('Oh no, you killed poor Morris! When he passed away from sleep deprivation after', i, '/', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif thirst > 100:
+            print('Oh no, you killed poor Morris! When he died of thirst after', i, '/', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif hunger > 100:
+            print('Oh no, you killed poor Morris! When he died of starvation after', i, '/', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif gold < 0:
+            print('Oh no, you bankrupted poor Morris! When you caused him to go broke after', i, '/', turns, 'turns, he had', gold, 'gold\n\n' 'His attributes were at the following levels:')
+            break
+        elif sleepiness >= 95:
+            sleepiness, thirst, hunger, whiskey, gold = sleep(sleepiness, thirst, hunger, whiskey, gold)
+        elif thirst >= 95:
+            if whiskey < 1:
+                sleepiness, thirst, hunger, whiskey, gold = buy_whiskey(sleepiness, thirst, hunger, whiskey, gold)
+            sleepiness, thirst, hunger, whiskey, gold = drink(sleepiness, thirst, hunger, whiskey, gold)
+        elif hunger >= 95:
+            sleepiness, thirst, hunger, whiskey, gold = eat(sleepiness, thirst, hunger, whiskey, gold)
+        else:
+            sleepiness, thirst, hunger, whiskey, gold = mine(sleepiness, thirst, hunger, whiskey, gold)
+
+    return sleepiness, thirst, hunger, whiskey, gold, turns, turns_done
 
 def main():
-    sleepiness = 0
-    thirst = 0
-    hunger = 0
-    whisky = 0
-    gold = 0
-    morris(5, sleepiness, thirst, hunger, whisky, gold)
+    sleepiness, thirst, hunger, whiskey, gold, turns, turns_done = morris_in_recovery(1000)
+    print('Morris ended the game with', gold, 'gold after', turns_done, 'turns.\n\n' 'His attributes were at the following levels:')
+    print_attributes(sleepiness, thirst, hunger, whiskey, gold)
+
 
 main()
-
-# Morris skal mine så meget som muligt, så medmindre han dør, så vil det være første prioritet i min funktion.
