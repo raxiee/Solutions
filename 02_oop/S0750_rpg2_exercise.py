ba2e225d18f1cc3cb3c49f32e81bb3be80e8f226
raxiee <pellehartig@hotmail.com>
2024-04-10 15:47:44 +0200

    very commit yes?

--- 02_oop/S0750_rpg2_exercise.py
+++ 02_oop/S0750_rpg2_exercise.py
@@ -31,17 +31,14 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> done
 Fortsæt derefter med den næste fil."""
 
+
 class Character:
     def __init__(self, name, health, attackpower):
         self.name = name
         self.max_health = health
         self._current_health = health
-        if self._current_health < 0:
-            self._current_health = 0
         self.attackpower = attackpower
         self.attackcooldown = 0
-        if self.attackcooldown < 0:
-            self.attackcooldown = 0
 
     def __repr__(self):
         return f'Name: {self.name}\nMax health: {self.max_health}\nCurrent health: {self._current_health}\nAttackpower: {self.attackpower}\n'
@@ -56,12 +53,11 @@ class Character:
 
     def get_hit(self, attackpower):
         self._current_health -= attackpower
+        if self._current_health < 0:
+            self._current_health = 0
 
     def get_healed(self, healpower):
         self._current_health += healpower
-
-    def polymorphed(self):
-        self.attackcooldown += 2
 
 
 class Healer(Character):
@@ -76,75 +72,100 @@ class Healer(Character):
 class Mage(Character):
     def __init__(self, name, health, attackpower):
         super().__init__(name, health, attackpower)
+        self.polymorph_cooldown = 0
+        if self.polymorph_cooldown < 0:
+            self.polymorph_cooldown = 0
 
     def polymorph(self, target):
-        print(
-            f'{self.name} polymorphs {target.name}! {target.name} is unable to perform an action for next two turns! '
-            f'Polymorph is now on cooldown for 5 turns.\n'
-        )
-
+        print(f'{self.name} polymorphs {target.name}! {target.name} is unable to perform an action for next two turns! '
+              f'Polymorph is now on cooldown for 5 turns.\n')
+        target.attackcooldown += 3
 
 class Hunter(Character):
     def __init__(self, name, health, attackpower, pet_name):
         super().__init__(name, health, attackpower)
         self.pet_name = pet_name
         self.pet_attackpower = self.attackpower // 2
+        self.bestial_wrath_cooldown = 0
+        if self.bestial_wrath_cooldown < 0:
+            self.bestial_wrath_cooldown = 0
 
     def hit(self, target):
-        print(
-            f"{self.name} strikes {target.name} for {self.attackpower} damage, and their pet, "
-            f"{self.pet_name}, strikes {target.name} for {self.pet_attackpower} damage!\n"
-        )
+        print(f"{self.name} strikes {target.name} for {self.attackpower} damage, and their pet, "
+              f"{self.pet_name}, strikes {target.name} for an additional {self.pet_attackpower} damage!\n")
         target.get_hit(self.pet_attackpower)
         target.get_hit(self.attackpower)
 
-
-    def bestial_wrath(self):
-        print(
-            f"{self.name} and {self.pet_name} are enraged by their inner beast, causing their damage to be doubled this turn!"
-            f"Bestial wrath is now on cooldown for 5 turns.\n"
-        )
-
+    def bestial_wrath(self, target):
+        print(f"{self.name} and {self.pet_name} are enraged by their inner beasts, causing their damage to be doubled this turn! "
+              f"Bestial wrath is now on cooldown for 5 turns.\n")
+        target.get_hit(self.pet_attackpower * 2)
+        target.get_hit(self.attackpower * 2)
+        print(f"{self.name} strikes {target.name} for {self.attackpower * 2} damage, and their pet, "
+              f"{self.pet_name}, strikes {target.name} for {self.pet_attackpower * 2} damage!\n")
 
 def battle(char1, char2):
     print(f"A battle between the fierce {char1.name} and the ruthless {char2.name} is about to commence...\n")
     turns = 0
-    polymorph_cooldown = 0
-    if polymorph_cooldown < 0:
-        polymorph_cooldown = 0
-    bestial_wrath_cooldown = 0
-    if bestial_wrath_cooldown < 0:
-        bestial_wrath_cooldown = 0
-    char1.attackcooldown -= 1
-    char2.attackcooldown -= 1
-    while char1._current_health and char2._current_health > 0:
-        if char1._current_health == 0:
-            print(f"{char1.name} has been defeated, and {char2.name} is victorious with "
-                  f"{char2._current_health} health remaining after {turns} turns! Are you not entertained?!?"
-                  )
-            break
-        if char2._current_health == 0:
-            print(f"{char2.name} has been defeated, and {char1.name} is victorious with "
-                  f"{char1._current_health} health remaining after {turns} turns! Are you not entertained?!?"
-                  )
-            break
-        if isinstance(char1, Mage) and polymorph_cooldown == 0 and char1.attackpower == 0:
-            char1.polymorph(char2)
-            polymorph_cooldown = 5
+    while char1._current_health > 0 and char2._current_health > 0:
+        if char1.attackcooldown > 0:
+            char1.attackcooldown -= 1
+        if char2.attackcooldown > 0:
+            char2.attackcooldown -= 1
+        if isinstance(char1, Mage) and char1.polymorph_cooldown > 0:
+            char1.polymorph_cooldown -= 1
+        if isinstance(char2, Mage) and char2.polymorph_cooldown > 0:
+            char2.polymorph_cooldown -= 1
+        if isinstance(char1, Hunter) and char1.bestial_wrath_cooldown > 0:
+            char1.bestial_wrath_cooldown -= 1
+        if isinstance(char2, Hunter) and char2.bestial_wrath_cooldown > 0:
+            char2.bestial_wrath_cooldown -= 1
 
-    turns += 1
+        # if char1._current_health <= 0:
+        #     print(f"{char1.name} has been defeated, and {char2.name} is victorious with "
+        #           f"{char2._current_health} health remaining after {turns} turns! Are you not entertained?!?")
+        #     break
+        #
+        # if char2._current_health <= 0:
+        #     print(f"{char2.name} has been defeated, and {char1.name} is victorious with "
+        #           f"{char1._current_health} health remaining after {turns} turns! Are you not entertained?!?")
+        #     break
+
+        if isinstance(char1, Mage) and char1.polymorph_cooldown == 0 and char1.attackcooldown == 0:
+            char1.polymorph(char2)
+            char1.polymorph_cooldown += 6
+        elif isinstance(char1, Hunter) and char1.bestial_wrath_cooldown == 0 and char1.attackcooldown == 0:
+            char1.bestial_wrath(char2)
+            char1.bestial_wrath_cooldown += 6
+        elif char1.attackcooldown == 0:
+            char1.hit(char2)
+        else:
+            print(f"{char1.name}'s attack is still on cooldown, so their turn is skipped.\n")
+
+        if isinstance(char2, Hunter) and char2.bestial_wrath_cooldown == 0 and char2.attackcooldown == 0:
+            char2.bestial_wrath(char1)
+            char2.bestial_wrath_cooldown += 6
+        elif isinstance(char2, Mage) and char2.polymorph_cooldown == 0 and char2.attackcooldown == 0:
+            char2.polymorph(char1)
+            char2.polymorph_cooldown += 6
+        elif char2.attackcooldown == 0:
+            char2.hit(char1)
+        else:
+            print(f"{char2.name}'s attack is still on cooldown, so their turn is skipped.\n")
+            print(f"Attack cooldown value = {char2.attackcooldown}")
+
+        turns += 1
+
+    print(f"The battle lasted {turns} turns! Are you not entertained?!?\n")
+    print(f"Final health of {char1.name}: {char1._current_health}")
+    print(f"Final health of {char2.name}: {char2._current_health}")
 
 
 
 def main():
-    char1 = Hunter('Timothy', 80, 10, 'Pedro')
-    char2 = Mage('Christopher', 80, 10)
-    char3 = Healer('Leona', 80, 10)
-    print(f"Christopher's current health: {char2._current_health}")
-    char1.hit(char2)
-    print(f"Christopher's current health: {char2._current_health}")
-    char3.heal(char2)
-    print(f"Christopher's current health: {char2._current_health}")
+    char_1 = Hunter('Timothy', 250, 10, 'Pedro')
+    char_2 = Mage('Christopher', 250, 16)
+    battle(char_1, char_2)
 
 
 main()
